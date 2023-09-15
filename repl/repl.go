472cfb2292a6004a983687641961597e7820f3a4
Anthony Sifontes <anthony.sifontes@gmail.com>
2023-09-15 04:46:43 -0500

    Update repl to use parser and handle errors.

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"athena/lexer"
-	"athena/token"
+	"athena/parser"
 )
 
 const PROMPT = ">> "
@@ -20,11 +20,25 @@ func Start(in io.Reader, out io.Writer) {
 			return
 		}
 
-		line := scnner.Text()
+		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! Athena ran into some trouble here.\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
